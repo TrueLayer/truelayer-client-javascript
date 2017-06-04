391f1020ecb5d7e02d14c06db52b2cb31c33e76f
Luca Martinetti <luca@luca.io>
2017-06-04 13:09:57 +0100

    simplify data unit tests

--- test/unit/data.spec.ts
+++ test/unit/data.spec.ts
@@ -1,12 +1,6 @@
-import { Constants } from "./../../src/v1/constants";
-import { Data } from "./../../src/v1/data";
+import { Constants } from "../../src/v1/constants";
+import { Data } from "../../src/v1/data";
 import { Fixtures } from "./fixtures";
-import { IResponse } from "./../../src/v1/interfaces/data/IResponse";
-import { ITransaction } from "./../../src/v1/interfaces/data/ITransaction";
-import { IAccount } from "./../../src/v1/interfaces/data/IAccount";
-import { IBalance } from "./../../src/v1/interfaces/data/IBalance";
-import { IInfo } from "./../../src/v1/interfaces/data/IInfo";
-import { IMe } from "./../../src/v1/interfaces/data/IMe";
 import * as request from "request-promise";
 import * as sinon from "sinon";
 import { test } from "ava";
@@ -19,6 +13,10 @@ const data = new Data();
 
 // Create sinon instance
 const mock = sinon.sandbox.create();
+
+test.afterEach((t) => {
+    mock.restore();
+});
 
 test("buildRequestOptions() - returns well formed request options - required params", async (t) => {
     t.plan(1);
@@ -38,60 +36,50 @@ test("buildRequestOptions() - returns well formed request options - all params",
     t.deepEqual(actual, expected, "Incorrect response object.");
 });
 
-test.afterEach((t) => {
-    mock.restore();
-});
-
-test.serial("stubbed request body for /Me endpoint is correctly parsed", async (t) => {
-    t.plan(4);
-    mock.stub(request, "get").returns(JSON.parse(fixtures.meResponse));
+test("stubbed request body for /Me endpoint is correctly parsed", async (t) => {
+    const expected = fixtures.meResponse;
+    mock.stub(request, "get").returns(JSON.stringify(expected));
     const actual = await data.getMe(fixtures.accessToken);
-    const expected: IResponse<IMe> = JSON.parse(fixtures.meResponse);
-    t.is(actual.success, expected.success, "Incorrect success value");
-    t.deepEqual(actual, expected, "Incorrect results.cliend_id value");
+    t.plan(1);
+    return t.deepEqual(actual, expected);
 });
 
 test.serial("stubbed request body for /Info endpoint is correctly parsed", async (t) => {
-    t.plan(7);
-    mock.stub(request, "get").returns(JSON.parse(fixtures.infoResponse));
+    const expected = fixtures.infoResponse;
+    mock.stub(request, "get").returns(JSON.stringify(expected));
     const actual = await data.getInfo(fixtures.accessToken);
-    const expected: IResponse<IInfo> = JSON.parse(fixtures.infoResponse);
-    t.is(actual.success, expected.success, "Incorrect success value");
-    t.deepEqual(actual, expected, "Incorrect results.addresses value");
+    t.plan(1);
+    return t.deepEqual(actual, expected);
 });
 
 test.serial("stubbed request body for /Accounts endpoint is correctly parsed", async (t) => {
-    t.plan(8);
-    mock.stub(request, "get").returns(JSON.parse(fixtures.accountsResponse));
+    const expected = fixtures.accountsResponse;
+    mock.stub(request, "get").returns(JSON.stringify(expected));
     const actual = await data.getAccounts(fixtures.accessToken);
-    const expected: IResponse<IAccount> = JSON.parse(fixtures.accountsResponse);
-    t.is(actual.success, expected.success, "Incorrect success value");
-    t.deepEqual(actual, expected, "Incorrect results.account_id value");
+    t.plan(1);
+    return t.deepEqual(actual, expected);
 });
 
 test.serial("stubbed request body for /Accounts/{id} endpoint.deepEqual correctly parsed", async (t) => {
-    t.plan(8);
-    mock.stub(request, "get").returns(JSON.parse(fixtures.accountResponse));
-    const actual = await data.getAccountInfo(fixtures.accessToken, "test_account_id");
-    const expected: IResponse<IAccount> = JSON.parse(fixtures.accountResponse);
-    t.is(actual.success, expected.success, "Incorrect success value");
-    t.deepEqual(actual, expected, "Incorrect results.account_id value");
+    const expected = fixtures.accountsResponse;
+    mock.stub(request, "get").returns(JSON.stringify(expected));
+    const actual = await data.getAccount(fixtures.accessToken, "test_account_id");
+    t.plan(1);
+    return t.deepEqual(actual, expected);
 });
 
 test.serial("stubbed request body for /Balance endpoint is correctly parsed", async (t) => {
-    t.plan(5);
-    mock.stub(request, "get").returns(JSON.parse(fixtures.balanceResponse));
+    const expected = fixtures.balanceResponse;
+    mock.stub(request, "get").returns(JSON.stringify(expected));
     const actual = await data.getBalance(fixtures.accessToken, "test_account_id");
-    const expected: IResponse<IBalance> = JSON.parse(fixtures.balanceResponse);
-    t.is(actual.success, expected.success, "Incorrect success value");
-    t.deepEqual(actual, expected, "Incorrect results.available value");
+    t.plan(1);
+    return t.deepEqual(actual, expected);
 });
 
 test.serial("stubbed request body for /Transactions endpoint is correctly parsed", async (t) => {
-    t.plan(15);
-    mock.stub(request, "get").returns(JSON.parse(fixtures.transactionsResponse));
+    const expected = fixtures.transactionsResponse;
+    mock.stub(request, "get").returns(JSON.stringify(expected));
     const actual = await data.getTransactions(fixtures.accessToken, "test_account_id", "2017-04-20", "2017-04-30");
-    const expected: IResponse<ITransaction> = JSON.parse(fixtures.transactionsResponse);
-    t.is(actual.success, expected.success, "Incorrect success value");
-    t.deepEqual(actual, expected, "Incorrect results.amount value");
+    t.plan(1);
+    return t.deepEqual(actual, expected);
 });
