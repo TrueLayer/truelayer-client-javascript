ef7ea82a96208cc9d32d157255d89237378c4abf
Luca Martinetti <luca@luca.io>
2017-06-04 13:08:05 +0100

    make some fields optional in interfaces

--- src/v1/interfaces/data/IAccount.ts
+++ src/v1/interfaces/data/IAccount.ts
@@ -3,14 +3,14 @@ export interface IAccount {
     account_id: string;
     account_type: string;
     display_name?: string;
-    description: string;
+    description?: string;
     currency: string;
     account_number: IAccountNumber;
 }
 
 export interface IAccountNumber {
     iban: string;
-    swift_bic: string;
+    swift_bic?: string;
     number: string;
     sort_code: string;
 }
--- src/v1/interfaces/data/ITransaction.ts
+++ src/v1/interfaces/data/ITransaction.ts
@@ -1,11 +1,8 @@
-import { IBalance } from "./IBalance";
-
 export interface ITransaction {
     timestamp: string;
     description: string;
     transaction_type: string;
     amount: number;
     currency: string;
-    balance: IBalance;
     meta: object;
 }
